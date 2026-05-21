c23ecb4358d3d5562c5ccff1bb716971d5cf975e
shreyas <dearvenumadhav@gmail.com>
2026-05-20 22:07:43 -0500

    added multiple recipients

--- send_email.py
+++ send_email.py
@@ -10,19 +10,22 @@ GMAIL_ADDRESS = os.environ["GMAIL_ADDRESS"]
 GMAIL_APP_PASSWORD = os.environ["GMAIL_APP_PASSWORD"]
 
 def send_email():
-    recipient = "kmadhav03@gmail.com"  # change this
+    recipients = [
+        "kmadhav03@gmail.com",
+        "dearvenumadhav@gmail.com"
+    ]
     subject = "Your Daily Email"
     body = "Hello! This is your scheduled email."
 
     msg = MIMEMultipart()
     msg["From"] = GMAIL_ADDRESS
-    msg["To"] = recipient
+    msg["To"] = ", ".join(recipients)
     msg["Subject"] = subject
     msg.attach(MIMEText(body, "plain"))
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(GMAIL_ADDRESS, GMAIL_APP_PASSWORD)
-        server.sendmail(GMAIL_ADDRESS, recipient, msg.as_string())
+        server.sendmail(GMAIL_ADDRESS, recipients, msg.as_string())
         print("Email sent successfully!")
 
     print(f"Script ran at: {datetime.datetime.now(datetime.UTC)} UTC")
